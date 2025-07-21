39f91e0c9b8bb78f28065aeff0ad94b58a3c01ec
pii <pii@pi-6500eb0>
2025-07-20 21:13:46 -0400

    AFC_variable.py modified - 07/20/2025 - 09:13:42 PM

--- printer_data/config/AFC/AFC_variable.py
+++ printer_data/config/AFC/AFC_variable.py
@@ -1,8 +1,8 @@
 [virtual_input my_filament_pin]
-initial_value: 1                     # 1 = filament detected at startup
+ initial_value: 1                     # 1 = filament detected at startup
 
 [virtual_filament_sensor my_sensor]
-pin: my_filament_pin                 # name of the virtual_input section
-pause_on_runout: True                # other filament sensor options work here
-# runout_gcode: ...
-# insert_gcode: ...
+ pin: my_filament_pin                 # name of the virtual_input section
+ pause_on_runout: True                # other filament sensor options work here
+ # runout_gcode: ...
+ # insert_gcode: ...
