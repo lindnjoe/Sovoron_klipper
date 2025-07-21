9e2aa71a7e28c4e480cb4dcb85ce9d76be96af6a
pii <pii@pi-6500eb0>
2025-07-20 20:30:45 -0400

    AFC_variable.py modified - 07/20/2025 - 08:30:41 PM

--- printer_data/config/AFC/AFC_variable.py
+++ printer_data/config/AFC/AFC_variable.py
@@ -3,3 +3,8 @@
 [virtual_input my_virtual_pin]
 initial_value: 1
 [virtual_filament_sensor]
+[virtual_filament_sensor my_filament_sensor]
+pin: my_virtual_pin        # name from the virtual_input section
+pause_on_runout: True      # optional, defaults to True
+runout_gcode: ...          # optional
+insert_gcode: ...          # optional
