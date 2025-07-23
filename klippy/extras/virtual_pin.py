19d6e1dd0a1736bc3b227bd29963b9a305c9e2e5
pii <pii@pi-6500eb0>
2025-07-22 21:02:41 -0400

    octoeverywhere.conf modified - 07/22/2025 - 09:02:34 PM

--- klippy/extras/virtual_pin.py
+++
@@ -1,9 +0,0 @@
-"""Legacy wrapper for virtual pins and filament sensors.
-
-This module re-exports the implementation from ``ams_pin.py`` and
-``filament_switch_sensor.py`` so existing configurations referencing
-``virtual_pin`` continue to work without modification.
-"""
-
-from .ams_pin import *
-from .filament_switch_sensor import VirtualSwitchSensor as VirtualFilamentSensor
