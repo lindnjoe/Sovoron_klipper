be9f401749ec2cb3807b744ec7a6ed7eec552de1
pii <pii@pi-6500eb0>
2025-07-22 22:25:17 -0400

    octoeverywhere.conf modified - 07/22/2025 - 10:25:10 PM

--- klippy/extras/virtual_input_pin.py
+++
@@ -1,8 +0,0 @@
-"""Legacy wrapper for virtual input pins.
-
-This module simply re-exports the implementation from ``ams_pin.py`` so
-existing configurations referencing ``virtual_input_pin`` continue to
-work.
-"""
-
-from .ams_pin import *
