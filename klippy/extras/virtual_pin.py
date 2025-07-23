f131feeaf9ade45822d0a570650cf5412fdb609c
pii <pii@pi-6500eb0>
2025-07-22 20:15:06 -0400

    New backup from 07/22/2025 - 08:15:05 PM

--- klippy/extras/virtual_pin.py
+++
@@ -1,9 +0,0 @@
-# Combined virtual pin and filament sensor module for Klipper
-#
-# Provides a software-defined input pin that can be attached anywhere an
-# endstop would normally be used, and optionally emulates a filament
-# switch sensor using that pin.
-#
-# Copyright (C) 2024  The Klipper Project
-# This file may be distributed under the terms of the GNU GPLv3 license.
-
