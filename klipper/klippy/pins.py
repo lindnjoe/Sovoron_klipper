211ecc6094cf5c852bad0cbf41b2b855f838f461
pii <pii@pi-6500eb0>
2025-07-26 08:44:19 -0400

    test_virtual_pin.py modified - 07/26/2025 - 08:44:14 AM

--- klipper/klippy/pins.py
+++ klipper/klippy/pins.py
@@ -135,10 +135,4 @@ class PrinterPins:
         self.allow_multi_use_pins[share_name] = True
 
 def add_printer_objects(config):
-    printer = config.get_printer()
-    printer.add_object('pins', PrinterPins())
-    # If the config defines any virtual input pins, ensure the virtual_pin
-    # chip is registered before other modules attempt to parse pins.
-    if config.get_prefix_sections('virtual_input_pin'):
-        from .extras import virtual_input_pin
-        virtual_input_pin.add_printer_objects(config)
+    config.get_printer().add_object('pins', PrinterPins())
