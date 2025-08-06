c4880b8ce713b67a8dc4fdeea74ba1942b21384e
pii <pii@pi-6500eb0>
2025-08-05 22:20:18 -0400

    virtual_input_pin.py modified - 08/05/2025 - 10:20:13 PM

--- klipper/klippy/extras/virtual_input_pin.py
+++ klipper/klippy/extras/virtual_input_pin.py
@@ -357,3 +357,5 @@ def add_printer_objects(config):
         printer.lookup_object('pins').register_chip('virtual_pin', chip)
     except pins.error:
         pass
+def load_config(config):
+    return VirtualInputPin(config)
