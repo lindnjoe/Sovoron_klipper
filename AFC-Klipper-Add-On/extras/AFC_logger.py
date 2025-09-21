e5024a3dd9d73b5b5278b4c10fd2268cff622cef
pii <pii@pi-56bf368>
2025-09-20 23:02:33 -0400

    New Backup on boot - 20/09/25 - 23:02:19

--- AFC-Klipper-Add-On/extras/AFC_logger.py
+++ AFC-Klipper-Add-On/extras/AFC_logger.py
@@ -4,6 +4,11 @@
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 
+try:
+    from .. import APP_NAME
+except:
+    APP_NAME = "Klipper"
+
 import logging
 import re
 import os
@@ -13,19 +18,16 @@ from webhooks import GCodeHelper
 
 class AFC_QueueListener(QueueListener):
     def __init__(self, filename):
-        try:
-            # Kalico needs an extra parameter passed in for log rollover
+        if APP_NAME == "Kalico":
             super().__init__(filename, False)
-        except:
+        else:
             super().__init__(filename)
 
         logging.handlers.TimedRotatingFileHandler.__init__(
             self, filename, when="S", interval=60 * 60 * 24, backupCount=5
         )
 
-        # Commenting out log rollover for now as it causes more of a hassle when getting users logs
-        # and causes information to disappear if a user restart alot
-        # logging.handlers.TimedRotatingFileHandler.doRollover(self)
+        logging.handlers.TimedRotatingFileHandler.doRollover(self)
 
 class AFC_logger:
     def __init__(self, printer, afc_obj):
@@ -33,6 +35,7 @@ class AFC_logger:
         self.afc     = afc_obj
         self.gcode   = printer.lookup_object('gcode')
         self.webhooks = printer.lookup_object('webhooks')
+        printer.register_event_handler( "gcode:request_restart", self._stop)
 
         log_path = printer.start_args['log_file']
         dirname = Path(log_path).parent
@@ -47,6 +50,9 @@ class AFC_logger:
         self.logger.addHandler(self.afc_queue_handler)
         self.logger.setLevel(logging.DEBUG)
         self.print_debug_console = False
+
+    def _stop(self, eventtime):
+        self.afc_ql.stop()
 
     def _add_monotonic(self, message):
         return "{:10.3f} {}".format(self.reactor.monotonic(), message)
@@ -73,14 +79,6 @@ class AFC_logger:
                 self.logger.info(self._format(line))
         self.send_callback(message)
 
-    def warning(self, message):
-        for line in message.lstrip().rstrip().split("\n"):
-            self.logger.debug(self._format("WARNING: {}".format(line)))
-
-        self.send_callback(f"<span class=warning--text>WARNING: {message}</span>")
-
-        self.afc.message_queue.append((message, "warning"))
-
     def debug(self, message, only_debug=False, traceback=None):
         for line in message.lstrip().rstrip().split("\n"):
             self.logger.debug(self._format("DEBUG: {}".format(line)))
@@ -91,6 +89,7 @@ class AFC_logger:
         if traceback is not None:
             for line in traceback.lstrip().rstrip().split("\n"):
                 self.logger.debug( self._format("DEBUG: {}".format(line)))
+
 
     def error(self, message, traceback=None, stack_name=""):
         """
