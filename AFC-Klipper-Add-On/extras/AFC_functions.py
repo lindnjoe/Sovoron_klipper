e0f9fec7333a02d00f5beada5f7c5b901ac6eac9
lindnjoe <32586000+lindnjoe@users.noreply.github.com>
2025-10-24 12:09:10 -0400

    Fix lane lookup during prints

--- AFC-Klipper-Add-On/extras/AFC_functions.py
+++ AFC-Klipper-Add-On/extras/AFC_functions.py
@@ -362,11 +362,15 @@ class afcFunction:
 
         :return string: Current lane name that is loaded, None if nothing is loaded
         """
-        if self.printer.state_message == 'Printer is ready':
-            current_extruder = self.get_current_extruder()
-            if current_extruder is not None:
-                return self.afc.tools[current_extruder].lane_loaded
-        return None
+        current_extruder = self.get_current_extruder()
+        if current_extruder is None:
+            return None
+
+        tool = self.afc.tools.get(current_extruder)
+        if tool is None:
+            return None
+
+        return tool.lane_loaded
 
     def get_current_lane_obj(self):
         """
