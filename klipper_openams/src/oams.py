f6cd736514e6e68e99d133999a17ca19535c8de7
pii <pii@pi-56bf368>
2025-11-08 21:44:43 -0500

    New Backup on boot - 08/11/25 - 21:44:31

--- klipper_openams/src/oams.py
+++ klipper_openams/src/oams.py
@@ -622,7 +622,13 @@ OAMS[%s]: current_spool=%s fps_value=%s f1s_hes_value_0=%d f1s_hes_value_1=%d f1
     def load_spool(self, spool_idx):
         self.action_status = OAMSStatus.LOADING
         self.oams_load_spool_cmd.send([spool_idx])
+        timeout = self.reactor.monotonic() + 30.0  # 30 second timeout
         while self.action_status is not None:
+            if self.reactor.monotonic() > timeout:
+                logging.error("OAMS[%d]: Load operation timed out after 30 seconds", self.oams_idx)
+                self.action_status = None
+                self.action_status_code = OAMSOpCode.ERROR_UNSPECIFIED
+                return False, "OAMS load operation timed out (MCU unresponsive)"
             self.reactor.pause(self.reactor.monotonic() + 0.1)
         if self.action_status_code == OAMSOpCode.SUCCESS:
             self.current_spool = spool_idx
@@ -654,7 +660,13 @@ OAMS[%s]: current_spool=%s fps_value=%s f1s_hes_value_0=%d f1s_hes_value_1=%d f1
     def unload_spool(self):
         self.action_status = OAMSStatus.UNLOADING
         self.oams_unload_spool_cmd.send()
+        timeout = self.reactor.monotonic() + 30.0  # 30 second timeout
         while self.action_status is not None:
+            if self.reactor.monotonic() > timeout:
+                logging.error("OAMS[%d]: Unload operation timed out after 30 seconds", self.oams_idx)
+                self.action_status = None
+                self.action_status_code = OAMSOpCode.ERROR_UNSPECIFIED
+                return False, "OAMS unload operation timed out (MCU unresponsive)"
             self.reactor.pause(self.reactor.monotonic() + 0.1)
         if self.action_status_code == OAMSOpCode.SUCCESS:
             self.current_spool = None
