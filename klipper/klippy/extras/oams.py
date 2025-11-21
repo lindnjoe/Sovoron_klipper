ae3caa1e0b557989ed125a75d308349cac47b1f9
lindnjoe <32586000+lindnjoe@users.noreply.github.com>
2025-11-21 15:22:40 -0500

    Replace logger.exception with logger.error

--- klipper/klippy/extras/oams.py
+++ klipper/klippy/extras/oams.py
@@ -92,7 +92,8 @@ class OAMS:
         self.hub_hes_is_above: bool = config.getboolean("hub_hes_is_above")
         
         # Physical configuration
-        self.filament_path_length: float = config.getfloat("ptfe_length")
+        self.ptfe_length: float = config.getfloat("ptfe_length")
+        self.filament_path_length: float = self.ptfe_length
         self.oams_idx: int = config.getint("oams_idx")
 
         # PID control parameters for pressure
