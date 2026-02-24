bba2c6f3bcb8e93f0f41d3704080207d32cd5c61
pii <pii@pi-56bf368>
2026-02-23 22:26:42 -0500

    New Backup on boot - 23/02/26 - 22:26:29

--- AFC-Klipper-Add-On/extras/AFC_functions.py
+++ AFC-Klipper-Add-On/extras/AFC_functions.py
@@ -5,7 +5,7 @@
 # This file may be distributed under the terms of the GNU GPLv3 license.
 #
 # This file includes code modified from the Shaketune Project. https://github.com/Frix-x/klippain-shaketune
-# Originally authored by F�lix Boisselier and licensed under the GNU General Public License v3.0.
+# Originally authored by Félix Boisselier and licensed under the GNU General Public License v3.0.
 #
 # Full license text available at: https://www.gnu.org/licenses/gpl-3.0.html
 from __future__ import annotations
@@ -1901,7 +1901,7 @@ class afcDeltaTime:
             curr_time = datetime.now()
             self.delta_time = (curr_time - self.last_time ).total_seconds()
             total_time = (curr_time - self.start_time).total_seconds()
-            msg = "{} (?t:{:.3f}s, t:{:.3f})".format( msg, self.delta_time, total_time )
+            msg = "{} (Δt:{:.3f}s, t:{:.3f})".format( msg, self.delta_time, total_time )
             if debug:
                 self.logger.debug( msg )
             else:
