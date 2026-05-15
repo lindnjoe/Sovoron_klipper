1802e4baeaaf8b4908b6ee4eab4751c3bd85b0c8
pii <pii@pi-56bf368>
2026-05-15 00:03:43 -0400

    New Backup on boot - 15/05/26 - 00:03:28

--- AFC-Klipper-Add-On/extras/AFC_prep.py
+++ AFC-Klipper-Add-On/extras/AFC_prep.py
@@ -37,20 +37,6 @@ class afcPrep:
         self.afc.gcode.register_command('PREP', self.PREP, desc=None)
         self.logger = self.afc.logger
 
-    def _rename(self, base_name, rename_name, rename_macro, rename_help):
-        """
-        Helper function to get stock macros, rename to something and replace stock macro with AFC functions
-        """
-        # Renaming users Resume macro so that RESUME calls AFC_Resume function instead
-        prev_cmd = self.afc.gcode.register_command(base_name, None)
-        if prev_cmd is not None:
-            pdesc = "Renamed builtin of '%s'" % (base_name,)
-            self.afc.gcode.register_command(rename_name, prev_cmd, desc=pdesc)
-        else:
-            self.logger.debug("{}Existing command {} not found in gcode_macros{}".format("<span class=warning--text>", base_name, "</span>",))
-        self.logger.debug("PREP-renaming macro {}".format(base_name))
-        self.afc.gcode.register_command(base_name, rename_macro, desc=rename_help)
-
     def _rename_macros(self):
         """
         Helper function to rename multiple macros and substitute with AFC macros.
@@ -61,12 +47,12 @@ class afcPrep:
         # Checking to see if rename has already been done, don't want to rename again if prep was already ran
         if not self.rename_occurred:
             self.rename_occurred = True
-            self._rename(self.afc.error.BASE_RESUME_NAME, self.afc.error.AFC_RENAME_RESUME_NAME, self.afc.error.cmd_AFC_RESUME, self.afc.error.cmd_AFC_RESUME_help)
-            self._rename(self.afc.error.BASE_PAUSE_NAME, self.afc.error.AFC_RENAME_PAUSE_NAME, self.afc.error.cmd_AFC_PAUSE, self.afc.error.cmd_AFC_RESUME_help)
+            self.afc.function._rename(self.afc.error.BASE_RESUME_NAME, self.afc.error.AFC_RENAME_RESUME_NAME, self.afc.error.cmd_AFC_RESUME, self.afc.error.cmd_AFC_RESUME_help)
+            self.afc.function._rename(self.afc.error.BASE_PAUSE_NAME, self.afc.error.AFC_RENAME_PAUSE_NAME, self.afc.error.cmd_AFC_PAUSE, self.afc.error.cmd_AFC_RESUME_help)
 
             # Check to see if the user does not want to rename UNLOAD_FILAMENT macro
             if not self.dis_unload_macro:
-                self._rename(self.afc.BASE_UNLOAD_FILAMENT, self.afc.RENAMED_UNLOAD_FILAMENT, self.afc.cmd_TOOL_UNLOAD, self.afc.cmd_TOOL_UNLOAD_help)
+                self.afc.function._rename(self.afc.BASE_UNLOAD_FILAMENT, self.afc.RENAMED_UNLOAD_FILAMENT, self.afc.cmd_TOOL_UNLOAD, self.afc.cmd_TOOL_UNLOAD_help)
 
     def _td1_prep(self, overrall_status):
         '''
@@ -191,21 +177,22 @@ class afcPrep:
                             cur_lane.filament_diameter= units[cur_lane.unit][cur_lane.name]["diameter"]
                         if 'empty_spool_weight' in units[cur_lane.unit][cur_lane.name]:
                             cur_lane.empty_spool_weight= units[cur_lane.unit][cur_lane.name]["empty_spool_weight"]
-                        if 'bed_temp' in units[cur_lane.unit][cur_lane.name]:
-                            cur_lane.bed_temp = units[cur_lane.unit][cur_lane.name]['bed_temp']
-                        if 'extruder_temp' in units[cur_lane.unit][cur_lane.name]:
-                            cur_lane.extruder_temp = units[cur_lane.unit][cur_lane.name]['extruder_temp']
+                        if 'bed_temp' in units[cur_lane.unit][cur_lane.name]: cur_lane.bed_temp = units[cur_lane.unit][cur_lane.name]['bed_temp']
+                        if 'extruder_temp' in units[cur_lane.unit][cur_lane.name]: cur_lane.extruder_temp = units[cur_lane.unit][cur_lane.name]['extruder_temp']
 
-                        # Convert numeric fields from saved vars (may be strings)
-                        for attr in ("bed_temp", "extruder_temp", "weight"):
-                            val = getattr(cur_lane, attr, None)
-                            if val is None or val == '' or str(val).upper() == 'NONE':
-                                setattr(cur_lane, attr, None if attr != "weight" else 0)
+                        for attr_name in ("bed_temp", "extruder_temp", "weight"):
+                            value = getattr(cur_lane, attr_name, None)
+                            if value == "" or value == "NONE":
+                                setattr(cur_lane, attr_name, None)
                             else:
                                 try:
-                                    setattr(cur_lane, attr, float(val))
+                                    if not isinstance(value, float):
+                                        if value:
+                                            setattr(cur_lane, attr_name, float(value))
+                                        else:
+                                            setattr(cur_lane, attr_name, 0)
                                 except (ValueError, TypeError):
-                                    setattr(cur_lane, attr, None if attr != "weight" else 0)
+                                    setattr(cur_lane, attr_name, 0)
 
                     if 'runout_lane' in units[cur_lane.unit][cur_lane.name]: cur_lane.runout_lane = units[cur_lane.unit][cur_lane.name]['runout_lane']
                     if cur_lane.runout_lane == '' or cur_lane.runout_lane == 'NONE': cur_lane.runout_lane = None
@@ -260,7 +247,7 @@ class afcPrep:
         # have selectors to make sure the selector is on the correct lane
         current_lane = self.afc.function.get_current_lane_obj()
         if current_lane is not None:
-            current_lane.unit_obj.select_lane(current_lane)
+            current_lane.unit_obj.select_lane(current_lane, True)
             current_lane.sync_to_extruder()
 
         # Restore previous bypass state if virtual bypass is active
